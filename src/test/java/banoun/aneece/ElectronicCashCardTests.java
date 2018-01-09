d3f8ed881b8dbe0e94951ae7185c3db96ab86832
aneecebanoun <java2ee5@gmail.com>
2018-01-09 07:21:00 +0000

    init

--- src/test/java/banoun/aneece/ElectronicCashCardTests.java
+++ src/test/java/banoun/aneece/ElectronicCashCardTests.java
@@ -11,15 +11,12 @@ import org.junit.runner.RunWith;
 import com.anarsoft.vmlens.concurrent.junit.ConcurrentTestRunner;
 import com.anarsoft.vmlens.concurrent.junit.ThreadCount;
 
-import banoun.aneece.exceptions.NoSufficientFundException;
 import banoun.aneece.model.Card;
 import banoun.aneece.model.Retailer;
 
 @RunWith(ConcurrentTestRunner.class)
 public class ElectronicCashCardTests {
 
-	boolean noSufficientFundException = false;
-	boolean sufficientFund = true;
 	int retailerNumber = 0;
 
 	Card gold = new Card("GOLD");
@@ -35,31 +32,20 @@ public class ElectronicCashCardTests {
 	@ThreadCount(100)
 	public void noSufficientFundTest() {
 		Retailer retailer = new Retailer("R" + (retailerNumber++));
-		try{
 			retailer.makeCardSale(gold, 100);	
-		}catch(NoSufficientFundException e){
-			noSufficientFundException = true;
-		}
 	}
 	
 	@Test
 	@ThreadCount(100)
 	public void sufficientFundTest() {
 		Retailer retailer = new Retailer("R" + (retailerNumber++));
-		try{
 			retailer.makeCardSale(platinum, 100);	
-		}catch(NoSufficientFundException e){
-			sufficientFund = false;
-		}
 	}
 	
 	@After
 	public void allDone() {
-//		assertTrue("Sufficient Fund ERROR", sufficientFund);
 		assertTrue("Sufficient Fund ERROR", 1 == platinum.getBalance());
-
 		assertTrue("No Sufficient Fund Exception", 99 == gold.getBalance());
-		assertTrue("No Sufficient Fund Exception", noSufficientFundException);
 	}
 
 }
